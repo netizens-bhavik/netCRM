a2c37b2c6e9c1ddb7fd94cfeefca5d60c5d380d4
netizens-devyanshi <netizens.devyanshi@gmail.com>
2024-05-01 12:11:05 +0530

    change array name of role

--- app/Services/UserServices.php
+++ app/Services/UserServices.php
@@ -215,7 +215,7 @@ class UserServices
         try {
             $user = User::find($userId);
             if ($user) {
-                $roles = $user->role;
+                $roles = $user->roles;
                 // $userRole = $user->getRoleNames();
                 // $user['role'] = $userRole;
                 // $roleList = Role::roles;
