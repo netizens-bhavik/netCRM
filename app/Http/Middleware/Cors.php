e24aa2acc643515c716223bd49d6b686e28fea57
Aniket Santra <aniket.s@netizens.email>
2025-01-31 17:08:03 +0530

    changes in cors config file and middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -13,16 +13,13 @@ class Cors
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
-        // Remove CORS headers
         $response = $next($request);
 
-        // Remove Access-Control-Allow-Origin header
-        $response->header('Access-Control-Allow-Origin', null);
-
-        // Remove Access-Control-Allow-Headers header
-        $response->header('Access-Control-Allow-Headers', null);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $response;
     }
