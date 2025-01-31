0851d88f35e8d198ceb36e33bd77469c6a82dad6
Aniket Santra <aniket.s@netizens.email>
2025-01-31 16:37:38 +0530

    changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias(['cors', App\Http\Middleware\Cors::class]);
+        $middleware->alias(['cors', \App\Http\Middleware\Cors::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
